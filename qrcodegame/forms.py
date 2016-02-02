644335a21326094643b4248f9a4e7ce11fd3a14d
Gwen <gfdipert@gmail.com>
2016-02-02 15:07:26 -0500

    Fixed branding

--- qrcodegame/forms.py
+++ qrcodegame/forms.py
@@ -5,5 +5,5 @@ class QRCode(forms.Form):
 	page_title = forms.CharField(label="Progress page title")
 	phrase = forms.CharField(label="The game phrase.  Use '/' to delimit phrase components.")
 	message_title = forms.CharField(label="Message title")
-	message = forms.CharField(label="Message to display under the phrase.")
-	completedurl = forms.URLField(label="Customizable Hyperlink text for completed phrase link.",required=False)
+	message = forms.CharField(label="Message to display under the phrase")
+	completedurl = forms.URLField(label="Customizable Hyperlink text for completed phrase link",required=False)
