2878d55d9a902272130052e8cfdf4df460f28718
Gwen <gfdipert@gmail.com>
2016-02-02 14:25:38 -0500

    Adjusted form text

--- qrcodegame/forms.py
+++ qrcodegame/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class QRCode(forms.Form):
 	guide_id = forms.IntegerField(label="Guide ID")
 	page_title = forms.CharField(label="Progress page title")
-	phrase = forms.CharField(label="The game phrase.  Use '/' to delimit phrase components, or specify your own with -d.")
+	phrase = forms.CharField(label="The game phrase.  Use '/' to delimit phrase components.")
 	message_title = forms.CharField(label="Message title")
 	message = forms.CharField(label="Message to display under the phrase.")
 	completedurl = forms.URLField(label="Customizable Hyperlink text for completed phrase link.",required=False)
