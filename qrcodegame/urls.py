6764aa06e3dc9e509e2abc8f647be35577c84a92
Gwen <gfdipert@gmail.com>
2016-02-02 14:10:20 -0500

    Fixed imports...again

--- qrcodegame/urls.py
+++ qrcodegame/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^qrcodegame/new/$', views.qr_code_game_new, name='qr_code_game_new'),
+    url(r'^qrcodegame/$', views.qr_code_game_new, name='qr_code_game_new'),
 ]
