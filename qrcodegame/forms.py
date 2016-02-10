44bdd6cf7b4b32b177589627a0a06035d80919c4
Gwen <gfdipert@gmail.com>
2016-02-09 20:08:49 -0500

    Added form validation

--- qrcodegame/forms.py
+++ qrcodegame/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 
 class QRCode(forms.Form):
+    
 	guide_id = forms.IntegerField(label="Guide ID")
 	page_title = forms.CharField(label="Progress page title")
 	phrase = forms.CharField(label="The game phrase.  Use '/' to delimit phrase components.")
 	message_title = forms.CharField(label="Message title")
 	message = forms.CharField(label="Message to display under the phrase")
-	completedurl = forms.URLField(label="Customizable Hyperlink text for completed phrase link",required=False)
+	completedurl = forms.URLField(label="Turns on form that user can submit information to once qr game is completed.",required=False)
+	completedtext = forms.CharField(label="Customizable Hyperlink text for completed phrase link.",required=False)
+	storagekey = forms.CharField(label="Storage key",initial='guide__GuideID__')
+	html = forms.CharField(label="HTML File Name",initial='qrgame.html')
+
+	def clean(self):
+		cleaned_data = super(QRCode, self).clean()
+		completedurl = cleaned_data.get('completedurl')
+		completedtext = cleaned_data.get('completedtext')
+		msg = "shit broke"
+		if "maude" not in completedurl:
+			 self.add_error('completedurl', msg)
--- qrcodegame/views.py
+++ qrcodegame/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
+from qrcode.main import QRCode
+import urllib
 from .forms import QRCode
 
 
 def qr_code_game_new(request):
     form = QRCode()
+    if form.is_valid():
+    	return render(request, 'qrcodegame/game_edit.html', {'form': form})
+    else:
+    	form=QRCode()
     return render(request, 'qrcodegame/game_edit.html', {'form': form})
+
+def submit(request):
+	#grab values from form
+	guideid = request.POST.get('guide_id')
+	pagetitle = request.POST.get('page_title')
+	phrase = request.POST.get('phrase')
+	messagetitle = request.POST.get('message_title')
+	message = request.POST.get('message')
+	completedurl = request.POST.get('completedurl')
+	completedtext = request.POST.get('completedtext')
+
+	storagekey = request.POST.get('storagekey')
+	if storagekey is None:
+		storagekey = 'guide__' + guideid + '__'
+	html = request.POST.get('html')
+	if html is None:
+		html = 'qrgame.html'
+
+
+
